2459393bb8fd944441cf139d1858cf655f209cfc
RubenHaisma <74268046+RubenHaisma@users.noreply.github.com>
2024-12-12 16:02:55 +0100

    centrered

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,70 +27,72 @@ export function Navbar() {
         </div>
 
         {/* Desktop Navigation */}
-        <NavigationMenu className="hidden md:flex">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={cn(
-                    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
-                    pathname === "/" && "bg-accent/50"
-                  )}
-                >
-                  Home
-                </NavigationMenuLink>
+        <div className="hidden md:flex flex-grow justify-center ml-14"> {/* Added ml-4 for margin-left */}
+          <NavigationMenu>
+            <NavigationMenuList className="space-x-4"> {/* Added space-x-4 for spacing */}
+              <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={cn(
+                "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+                pathname === "/" && "bg-accent/50"
+              )}
+            >
+              Home
+            </NavigationMenuLink>
+          </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+          <NavigationMenuTrigger>Diensten</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              {services.map((service) => (
+                <li key={service.title}>
+            <NavigationMenuLink asChild>
+              <Link
+                href={service.href}
+                className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+              >
+                <div className="text-sm font-medium leading-none">
+                  {service.title}
+                </div>
+                <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                  {service.description}
+                </p>
               </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Diensten</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                  {services.map((service) => (
-                    <li key={service.title}>
-                      <NavigationMenuLink asChild>
-                        <Link
-                          href={service.href}
-                          className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                        >
-                          <div className="text-sm font-medium leading-none">
-                            {service.title}
-                          </div>
-                          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                            {service.description}
-                          </p>
-                        </Link>
-                      </NavigationMenuLink>
-                    </li>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/portfolio" legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={cn(
-                    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
-                    pathname === "/portfolio" && "bg-accent/50"
-                  )}
-                >
-                  Portfolio
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              {/* <Link href="/blog" legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={cn(
-                    "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
-                    pathname === "/blog" && "bg-accent/50"
-                  )}
-                >
-                  Blog
-                </NavigationMenuLink>
-              </Link> */}
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+            </NavigationMenuLink>
+                </li>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+          <Link href="/portfolio" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={cn(
+                "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+                pathname === "/portfolio" && "bg-accent/50"
+              )}
+            >
+              Portfolio
+            </NavigationMenuLink>
+          </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+          {/* <Link href="/blog" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={cn(
+                "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+                pathname === "/blog" && "bg-accent/50"
+              )}
+            >
+              Blog
+            </NavigationMenuLink>
+          </Link> */}
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
 
         {/* Mobile Navigation and Theme Toggle */}
         <div className="flex md:hidden items-center space-x-2">
